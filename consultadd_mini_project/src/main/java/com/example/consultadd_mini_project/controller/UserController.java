04a4130d75cf79c9590e55e534027538265876b4
prajwal-huggi <prajwalhuggi02@gmail.com>
2025-07-28 10:03:24 -0400

    Logout feature added

--- consultadd_mini_project/src/main/java/com/example/consultadd_mini_project/controller/UserController.java
+++ consultadd_mini_project/src/main/java/com/example/consultadd_mini_project/controller/UserController.java
@@ -4,7 +4,9 @@ import com.example.consultadd_mini_project.DTO.LoginResponseDTO;
 import com.example.consultadd_mini_project.DTO.ResponseDTO;
 import com.example.consultadd_mini_project.model.User;
 import com.example.consultadd_mini_project.service.userService.LoginUserService;
+import com.example.consultadd_mini_project.service.userService.LogoutUserService;
 import com.example.consultadd_mini_project.service.userService.RegisterUserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,13 +21,21 @@ public class UserController {
     @Autowired
     LoginUserService loginUserService;
 
+    @Autowired
+    LogoutUserService logoutUserService;
+
     @PostMapping("/login")
     public ResponseEntity<ResponseDTO<LoginResponseDTO>> getLogin(@RequestBody User user){
         return loginUserService.loginUser(user);
     }
 
     @PostMapping("/register")
-    public ResponseEntity<ResponseDTO<Object>> registeruser(@RequestBody User user){
+    public ResponseEntity<ResponseDTO<Object>> registerUser(@RequestBody User user){
         return registerUserService.registerUser(user);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<ResponseDTO<String>> logout(HttpServletRequest http){
+        return logoutUserService.logoutUser(http);
     }
 }
